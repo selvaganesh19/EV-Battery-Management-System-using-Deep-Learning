58437f4c47bad9d44530b0821a8c5d0383b49eae
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-08-05 13:28:37 +0530

    Added static folder

--- frontend/script.js
+++ frontend/script.js
@@ -82,7 +82,7 @@ async function handleFormSubmit(e) {
         const formData = new FormData();
         formData.append('vehicle_type', vehicleType);
         
-        const response = await fetch('http://127.0.0.1:8000/predict/', {
+        const response = await fetch('https://evbackend-fsw9.onrender.com/predict/', {
             method: 'POST',
             body: formData
         });
